0339a2796f08534ca53ec1d5b8b7cc6297b20412
Nirlan Souza <nirlan858@gmail.com>
2021-06-07 13:47:51 -0300

    functions extraction from int main()

--- forca.cpp
+++ forca.cpp
@@ -31,23 +31,23 @@ bool nao_enforcou() {
     return chutes_errados.size() < 5;
 }
 
-int main() {
-    setlocale(LC_ALL, "Portuguese");
-
+void imprime_cabecalho() {
     cout << "*********************" << endl;
     cout << "*** Jogo da Forca ***" << endl;
     cout << "*********************" << endl;
     cout << endl;
+}
 
-    while(nao_acertou() && nao_enforcou()) {
-
-        cout << "Chutes errados: ";
+void imprime_erros() {
+    cout << "Chutes errados: ";
         for(char letra : chutes_errados) {
             cout << letra << " ";
         }
-        cout << endl;
+        cout << endl;    
+}
 
-        for(char letra : PALAVRA_SECRETA) {
+void imprime_palavra() {
+    for(char letra : PALAVRA_SECRETA) {
             if(chutou[letra]) {
                 cout << letra << " ";
             }
@@ -56,8 +56,10 @@ int main() {
             }
         }
         cout << endl;
+}
 
-        cout << "Seu chute: ";
+void chuta() {
+    cout << "Seu chute: ";
         char chute;
         cin >> chute;
 
@@ -71,5 +73,27 @@ int main() {
             chutes_errados.push_back(chute);
         }
         cout << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    imprime_cabecalho();
+
+    while(nao_acertou() && nao_enforcou()) {
+        imprime_erros();
+
+        imprime_palavra();
+
+        chuta();
     }
+
+    cout << "Fim de jogo!" << endl;
+    cout << "A palavra secreta era: " << PALAVRA_SECRETA << endl;
+    if(nao_acertou()) {
+        cout << "Você perdeu! Tente novamente!" << endl;
+    }
+    else {
+        cout << "Parabéns! Você acertou a palavra secreta!" << endl;
+    }
+    
 }
