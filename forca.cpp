5b851712e1cc42c813061f85bc000e0287dd6dcf
Nirlan Souza <nirlan858@gmail.com>
2021-06-07 03:23:55 -0300

    print already found characters

--- forca.cpp
+++ forca.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include <locale>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_existe(char chute) {    
     for(char letra : PALAVRA_SECRETA) {
@@ -17,14 +19,24 @@ bool letra_existe(char chute) {
 int main() {
     setlocale(LC_ALL, "Portuguese");
     
-    cout << PALAVRA_SECRETA << endl;
-
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
     while(nao_acertou && nao_enforcou) {
+        for(char letra : PALAVRA_SECRETA) {
+            if(chutou[letra]) {
+                cout << letra << " ";
+            }
+            else {
+                cout << "_ ";
+            }
+        }
+        cout << endl;
+        
         char chute;
         cin >> chute;
+
+        chutou[chute] = true;
 
         if(letra_existe(chute)) {
             cout << "Você acertou! Seu chute está na palavra." << endl;            
