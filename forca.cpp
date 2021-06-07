f51b67f8af58d6e535afe5d59b830c4b01402f13
Nirlan Souza <nirlan858@gmail.com>
2021-06-07 02:24:22 -0300

    range-based loop to check if the character belongs to the secret word

--- forca.cpp
+++ forca.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
+#include <locale>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute) {    
+    for(char letra : PALAVRA_SECRETA) {
+        if(chute == letra) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
-    string palavra_secreta = "MELANCIA";
-    cout << palavra_secreta << endl;
+    setlocale(LC_ALL, "Portuguese");
+    
+    cout << PALAVRA_SECRETA << endl;
 
     bool nao_acertou = true;
     bool nao_enforcou = true;
@@ -13,6 +26,11 @@ int main() {
         char chute;
         cin >> chute;
 
-        cout << "O seu chute foi: " << chute << endl;
+        if(letra_existe(chute)) {
+            cout << "Você acertou! Seu chute está na palavra." << endl;            
+        }
+        else {
+            cout << "Você errou! Seu chute não está na palavra." << endl;
+        }
     }
 }
