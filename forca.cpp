8047e403e3b193952b3d7f38d31f96cb6ad482de
Nirlan Souza <nirlan858@gmail.com>
2021-06-07 14:20:47 -0300

    open external file to get the words

--- forca.cpp
+++ forca.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <fstream>
 #include <locale>
 using namespace std;
 
@@ -75,9 +76,27 @@ void chuta() {
         cout << endl;
 }
 
+void le_arquivo() {
+    ifstream arquivo;
+    arquivo.open("palavras.txt");
+    
+    int quantidade_palavras;
+    arquivo >> quantidade_palavras;
+
+    cout << "O arquivo possui " << quantidade_palavras << " palavras." << endl;
+
+    for(int i = 0; i < quantidade_palavras; i++) {
+        string palavra_lida;
+        arquivo >> palavra_lida;
+        cout << "Na linha " << i << " : " << palavra_lida << endl;
+    }
+}
+
 int main() {
     setlocale(LC_ALL, "Portuguese");
     imprime_cabecalho();
+
+    le_arquivo();
 
     while(nao_acertou() && nao_enforcou()) {
         imprime_erros();
