0a729dd98d2f991ed80c11985b30e4a5236f1c91
Nirlan Souza <nirlan858@gmail.com>
2021-06-07 04:16:38 -0300

    Print wrong guesses

--- forca.cpp
+++ forca.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 #include <locale>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
 map<char, bool> chutou;
+vector<char> chutes_errados;
 
 bool letra_existe(char chute) {    
     for(char letra : PALAVRA_SECRETA) {
@@ -18,11 +20,25 @@ bool letra_existe(char chute) {
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
+
+    cout << "*********************" << endl;
+    cout << "*** Jogo da Forca ***" << endl;
+    cout << "*********************" << endl;
+    cout << endl;
+    
     
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
-    while(nao_acertou && nao_enforcou) {
+    while(nao_acertou && nao_enforcou) {        
+
+        cout << "Chutes errados: ";
+        for(char letra : chutes_errados) {
+            cout << letra << " ";
+        }
+        cout << endl;
+
+        
         for(char letra : PALAVRA_SECRETA) {
             if(chutou[letra]) {
                 cout << letra << " ";
@@ -32,6 +48,8 @@ int main() {
             }
         }
         cout << endl;
+
+        cout << "Seu chute: ";
         
         char chute;
         cin >> chute;
@@ -43,6 +61,8 @@ int main() {
         }
         else {
             cout << "Você errou! Seu chute não está na palavra." << endl;
+            chutes_errados.push_back(chute);
         }
+        cout << endl;
     }
 }
